15bc9cb405a9138c62b4b456d0fddc4b3568ac78
nleitefaria <nleitefaria@gmail.com>
2023-01-25 14:42:33 +0000

    feature1: Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home-component', component: HomeComponent },
+    { path: 'artworks-component', component: ArtworksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
