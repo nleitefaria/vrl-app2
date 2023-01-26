a2a0b75afef581ba5a0b0e01ea9cfa80fdcd8847
nleitefaria <nleitefaria@gmail.com>
2023-01-26 11:29:53 +0000

    Added pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/artworks/artworks.component.ts
+++ src/app/artworks/artworks.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtworksService } from '../services/artworks.service';
 
 @Component({
   selector: 'app-artworks',
   templateUrl: './artworks.component.html',
   styleUrls: ['./artworks.component.css']
 })
-export class ArtworksComponent {
+export class ArtworksComponent implements OnInit {
+  artworks:any;
+  p: number = 1;
+  total: number = 0;
+
+  constructor(private service:ArtworksService) {}
+
+
+  ngOnInit() {
+    this.getArtworks();
+
+    alert(this.artworks);
+    alert(this.total);
+  }
+
+  getArtworks() {
+    this.service.getArtworks(this.p)
+        .subscribe((response: any) => {
+          this.artworks = response.data;
+          this.total = response.pagination.total;
+        });
+  }
+
+  pageChangeEvent(event: number){
+    this.p = event;
+    this.getArtworks();
+  }
 
 }
